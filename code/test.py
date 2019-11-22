871a5b111a87a23963f98ee61c00bf1a35fe1bbb
Anthea Li <chentisch@gmail.com>
2019-11-21 18:53:19 -0800

    reformulate files

--- code/test.py
+++ code/test.py
@@ -8,6 +8,7 @@
 ## LICENSE file in the root directory of this source tree 
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+
 import os
 import sys
 import time
@@ -22,10 +23,10 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 
-import utils
-from net import Net, Vgg16
+from utils import utils
+from model import Net, Vgg16
 
-from option import Options
+from global_var import Options
 
 
 
@@ -151,9 +152,33 @@ def fast_evaluate(args, basedir, contents, idx = 0):
             print(filename)
 
 # figure out the experiments type
-args = Options().parse()
-if args.subcommand is None:
-    raise ValueError("ERROR: specify the experiment type")
-if args.cuda and not torch.cuda.is_available():
+parser = argparse.ArgumentParser()
+eval_arg = parser.add_argument_group('Test')
+eval_arg.add_argument("--ngf", type=int, default=128,
+                                help="number of generator filter channels, default 128")
+eval_arg.add_argument("--content-image", type=str, required=True,
+                                help="path to content image you want to stylize")
+eval_arg.add_argument("--style-image", type=str, default="images/9styles/candy.jpg",
+                                help="path to style-image")
+eval_arg.add_argument("--content-size", type=int, default=512,
+                                help="factor for scaling down the content image")
+eval_arg.add_argument("--style-size", type=int, default=512,
+                                help="size of style-image, default is the original size of style image")
+eval_arg.add_argument("--style-folder", type=str, default="images/9styles/",
+                                help="path to style-folder")
+eval_arg.add_argument("--output-image", type=str, default="output.jpg",
+                                help="path for saving the output image")
+eval_arg.add_argument("--model", type=str, required=True,
+                                help="saved model to be used for stylizing the image")
+eval_arg.add_argument("--cuda", type=int, default=1,
+                                help="set it to 1 for running on GPU, 0 for CPU")    
+eval_arg.add_argument("--vgg-model-dir", type=str, default="models/",
+                                help="directory for vgg, if model is not present in the directory it is downloaded")
+parser.parse_args()
+
+
+# if args.subcommand is None:
+    # raise ValueError("ERROR: specify the experiment type")
+if eval_arg.cuda and not torch.cuda.is_available():
     raise ValueError("ERROR: cuda is not available, try running on CPU")
-evaluate(args)
+evaluate(eval_arg)
--- code/train.py
+++ code/train.py
@@ -12,6 +12,7 @@ import os
 import sys
 import time
 import numpy as np
+import argparse
 from tqdm import tqdm, trange
 
 import torch
@@ -22,7 +23,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 
-import utils
+from utils import utils
 from model import Net, Vgg16
 
 from global_var import Options
@@ -211,13 +212,48 @@ def check_paths(args):
         sys.exit(1)
 
 
-args = Options().parse()
-if args.subcommand is None:
-    raise ValueError("ERROR: specify the experiment type")
-if args.cuda and not torch.cuda.is_available():
+parser = argparse.ArgumentParser()
+train_arg = parser.add_argument_group('Train')
+train_arg.add_argument("--ngf", type=int, default=128,
+                                help="number of generator filter channels, default 128")
+train_arg.add_argument("--epochs", type=int, default=2,
+                                help="number of training epochs, default is 2")
+train_arg.add_argument("--batch-size", type=int, default=4,
+                                help="batch size for training, default is 4")
+train_arg.add_argument("--dataset", type=str, default="dataset/",
+                                help="path to training dataset, the path should point to a folder "
+                                "containing another folder with all the training images")
+train_arg.add_argument("--style-folder", type=str, default="images/9styles/",
+                                help="path to style-folder")
+train_arg.add_argument("--vgg-model-dir", type=str, default="models/",
+                                help="directory for vgg, if model is not present in the directory it is downloaded")
+train_arg.add_argument("--save-model-dir", type=str, default="models/",
+                                help="path to folder where trained model will be saved.")
+train_arg.add_argument("--image-size", type=int, default=256,
+                                help="size of training images, default is 256 X 256")
+train_arg.add_argument("--style-size", type=int, default=512,
+                                help="size of style-image, default is the original size of style image")
+train_arg.add_argument("--cuda", type=int, default=1, 
+                                help="set it to 1 for running on GPU, 0 for CPU")
+train_arg.add_argument("--seed", type=int, default=42, 
+                                help="random seed for training")
+train_arg.add_argument("--content-weight", type=float, default=1.0,
+                                help="weight for content-loss, default is 1.0")
+train_arg.add_argument("--style-weight", type=float, default=5.0,
+                                help="weight for style-loss, default is 5.0")
+train_arg.add_argument("--lr", type=float, default=1e-3,
+                                help="learning rate, default is 0.001")
+train_arg.add_argument("--log-interval", type=int, default=500,
+                                help="number of images after which the training loss is logged, default is 500")
+train_arg.add_argument("--resume", type=str, default=None,
+                                help="resume if needed")
+# args = Options().parse()
+# if args.subcommand is None:
+    # raise ValueError("ERROR: specify the experiment type")
+if train_arg.cuda and not torch.cuda.is_available():
     raise ValueError("ERROR: cuda is not available, try running on CPU")
 
 
-if args.subcommand == "train":
+# if args.subcommand == "train":
     # Training the model 
-    train(args)
+train(train_arg)
