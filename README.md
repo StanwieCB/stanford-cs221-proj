dd061b46ec1e3dd427be28d0447787afd6d89b7d
Hank Zhang <hankzhangcb@gmail.com>
2019-12-11 17:13:56 -0800

    clean

--- README.md
+++
@@ -1,2 +0,0 @@
-# stanford-cs221-proj
-Stanford CS 221 final project Fall 2019
--- code/models/download_model.sh
+++
@@ -1,3 +0,0 @@
-cd models
-wget -O 21styles.model https://www.dropbox.com/s/2iz8orqqubrfrpo/21styles.model?dl=1
-cd ..
